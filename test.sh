1720ba04c4bcb3883e9ba8c52b38599d6b1d7589
qjh <91476077+junhaiqi@users.noreply.github.com>
2025-11-19 00:16:24 +0800

    Update test.sh for testing wsd

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-make
+make -j8
 ./wsd example/test_ref.fa -m example/test_tems.fa > example/test_user_test_out.txt
 
 if cmp --silent "example/test_user_test_out.txt" "example/test_ref_out.txt"; then
